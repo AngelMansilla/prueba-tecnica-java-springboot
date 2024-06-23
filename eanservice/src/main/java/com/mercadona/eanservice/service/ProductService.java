90f9798c75b20b907797b97801da2f6ee47fead0
Angel Mansilla Puerto <angelmansillapuerto@outlook.com>
2024-06-23 12:42:29 +0200

    Fix: added ProductDTO in ProductService

--- eanservice/src/main/java/com/mercadona/eanservice/service/ProductService.java
+++ eanservice/src/main/java/com/mercadona/eanservice/service/ProductService.java
@@ -1,12 +1,14 @@
 package com.mercadona.eanservice.service;
 
+import com.mercadona.eanservice.dto.ProductDTO;
 import com.mercadona.eanservice.model.Product;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
     List<Product> findAll();
-    Optional<Product> findById(Long id);
+    Optional<ProductDTO> findById(Long id);
     Product save(Product product);
     void deleteById(Long id);
 }
